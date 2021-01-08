e15a5be2b1c4a92245ff1a1e603d19ddf31e9e26
QasemJaber <68447225+QasemJaber@users.noreply.github.com>
2021-01-08 09:20:22 -0600

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -1,17 +1,29 @@
-#!/bin/bash
+File Edit Options Buffers Tools Sh-Script Help                                                                                                  
+#!/bin/bash                                                                                                                                     
 
 condition=$1
 
+#Adding files to GitHub                                                                                                                         
 if [ $condition == "add" ]; then
-    read -p "Enter file name: " filename
+    read -p "Enter the file you wish to add to GitHub: " filename
     git add $filename
+    git commit
     git push
-    
+
+#Removing Files from GitHub                                                                                                                     
 elif [ $condition == "rm" ]; then
-    read -p "Enter file name: " filename
+    read -p "Enter the file you wish to delete from GitHub: " filename
     git rm $filename
+    git commit
     git push
-    
+
 else
     echo 'Sorry, I do not understand this command'
 fi
+
+
+
+
+
+
+
