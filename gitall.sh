7c9bd4942969d825199697431196974eeb5b9625
QasemJaber <68447225+QasemJaber@users.noreply.github.com>
2021-01-08 09:17:41 -0600

    Update gitall.sh

--- gitall.sh
+++ gitall.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-
-filename=$1
-
-git add $filename
-git commit
-git push
-
-sleep 1
-
-read -p "Enter tag name: " tagname
-git tag --delete $tagname
-git push --delete origin $tagname 
-git tag $tagname
-git push origin $tagname
+#!/bin/bash                                                                                                                                     
+                                                                                                                                                
+filename=$1                                                                                                                                     
+                                                                                                                                                
+#Add and push the file                                                                                                                          
+git add $filename                                                                                                                               
+git commit                                                                                                                                      
+git push                                                                                                                                        
+                                                                                                                                                
+#Create a tag/update the tag for the file                                                                                                       
+read -p "Enter tag name: " tagname                                                                                                              
+git tag --delete $tagname                                                                                                                       
+git push --delete origin $tagname                                                                                                               
+git tag $tagname                                                                                                                                
+git push origin $tagname 
