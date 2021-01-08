ecdbe6ff94dfcd608b5b3ba92fbcc773f31a49cf
QasemJaber <68447225+QasemJaber@users.noreply.github.com>
2021-01-08 09:21:21 -0600

    Update gittag.sh

--- gittag.sh
+++ gittag.sh
@@ -1,23 +1,26 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                     
 
 condition=$1
 
+#Updating the specified tag                                                                                                                     
 if [ $condition == "update" ]; then
-    read -p "Enter tag name: " tagname
+    read -p "Enter the tag you wish to be updated: " tagname
     git tag --delete $tagname
     git push --delete origin $tagname
     git tag $tagname
     git push origin $tagname
     echo 'The tag has been successfully updated.'
 
+#Deleteting the specified tag                                                                                                                   
 elif [ $condition == "delete" ]; then
-    read -p "Enter tag name: " tagname
+    read -p "Enter the tag you wish to be deleted: " tagname
     git tag --delete $tagname
     git push --delete origin $tagname
     echo 'The tag has been successfully deleted.'
 
+#Creating the specified tag                                                                                                                     
 elif [ $condition == "tag" ]; then
-    read -p "Enter tag name: " tagname
+    read -p "Enter the tag you wish to be created: " tagname
     git tag $tagname
     git push origin $tagname
     echo 'The tag has been successfully created.'
